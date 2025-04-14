8d056108dba18fab4db359ba39cb80cf149c93ef
“twnmom313” <bjs20390@email.vccs.edu>
2025-04-14 08:41:43 -0400

    updates

--- script.js
+++ script.js
@@ -1,123 +1,3 @@
-//carousel graphics
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("demo");
-  let captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
-
-//carousel animation
-let slideIndexAnimation = 1;
-showSlides(slideIndexAnimation);
-
-function plusSlides(n) {
-  showSlides(slideIndexAnimation += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndexAnimation = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlidesAnimation");
-  let dots = document.getElementsByClassName("demoAnimation");
-  let captionText = document.getElementById("captionAnimation");
-  if (n > slides.length) {slideIndexAnimation = 1}
-  if (n < 1) {slideIndexAnimation = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace("active", "");
-  }
-  slides[slideIndexAnimation-1].style.display = "block";
-  dots[slideIndexAnimation-1].className += "active";
-  captionText.innerHTML = dots[slideIndexAnimation-1].alt;
-}
-
-//carousel photography
-let slideIndexPhoto = 1;
-showSlides(slideIndexPhoto);
-
-function plusSlides(n) {
-  showSlides(slideIndexPhoto += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndexPhoto = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlidesPhoto");
-  let dots = document.getElementsByClassName("demoPhoto");
-  let captionText = document.getElementById("captionPhoto");
-  if (n > slides.length) {slideIndexPhoto = 1}
-  if (n < 1) {slideIndexPhoto = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndexPhoto-1].style.display = "block";
-  dots[slideIndexPhoto-1].className += " active";
-  captionText.innerHTML = dots[slideIndexPhoto-1].alt;
-}
-
-//carousel web design
-let slideIndexWeb = 1;
-showSlides(slideIndexWeb);
-
-function plusSlides(n) {
-  showSlides(slideIndexWeb += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndexWeb = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlidesWeb");
-  let dots = document.getElementsByClassName("demoWeb");
-  let captionText = document.getElementById("captionWeb");
-  if (n > slides.length) {slideIndexWeb = 1}
-  if (n < 1) {slideIndexWeb = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndexWeb-1].style.display = "block";
-  dots[slideIndexWeb-1].className += " active";
-  captionText.innerHTML = dots[slideIndexWeb-1].alt;
-}
-
 //dark mode
 const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
 
@@ -139,4 +19,157 @@ if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').match
   logoImage.src = "images/3.3_Soto_Logo.png";
 } else {
   logoImage.src = "images/3.3_Soto_Logo_R.png";
+}
+
+
+// tabs
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+
+// === GRAPHIC DESIGN SLIDESHOW ===
+let slideIndexG = 1;
+showSlides(slideIndexG);
+
+function plusSlides(n) {
+  showSlides(slideIndexG += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndexG = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndexG = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndexG-1].style.display = "block";
+  dots[slideIndexG-1].className += " active";
+  captionText.innerHTML = dots[slideIndexG-1].alt;
+}
+
+// === ANIMATION SLIDESHOW ===
+let slideIndexA = 1;
+showSlides(slideIndexA);
+
+function plusSlides(n) {
+  showSlides(slideIndexA += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndexA = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndexA = 1}
+  if (n < 1) {slideIndexA = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndexA-1].style.display = "block";
+  dots[slideIndexA-1].className += " active";
+  captionText.innerHTML = dots[slideIndexA-1].alt;
+}
+
+// === PHOTOGRAPHY SLIDESHOW ===
+let slideIndexP = 1;
+showSlides(slideIndexP);
+
+function plusSlides(n) {
+  showSlides(slideIndexP += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndexP = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndexP = 1}
+  if (n < 1) {slideIndexP = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndexP-1].style.display = "block";
+  dots[slideIndexP-1].className += " active";
+  captionText.innerHTML = dots[slideIndexP-1].alt;
+}
+
+// === WEB DESIGN/DEVELOPMENT SLIDESHOW ===
+let slideIndexW = 1;
+showSlides(slideIndexW);
+
+function plusSlides(n) {
+  showSlides(slideIndexW += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndexW = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndexW = 1}
+  if (n < 1) {slideIndexW = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndexW-1].style.display = "block";
+  dots[slideIndexW-1].className += " active";
+  captionText.innerHTML = dots[slideIndexW-1].alt;
+}
+
+//TESTIMONIALS
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
 }
